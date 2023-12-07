d87c98d414be2547ce81bb5ea2c3010d78e7765c
Mayank Kumar <96490105+mank-423@users.noreply.github.com>
2023-12-07 13:42:27 +0530

    Authentication UI done

--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import icon from '../../assets/icon.png'
 import './Auth.css'
+import AboutAuth from './AboutAuth';
 
 
 const Auth = () => {
@@ -14,12 +15,14 @@ const Auth = () => {
 
   return (
     <section className='auth-section'>
-      <div className='aith-container'>
+      {isSignUp && <AboutAuth />}
+      
+      <div className='auth-container-2'>
         {!isSignUp && (
           <img src={icon} alt="stackoverflow" className='login-logo' />
         )}
 
-        <form>
+        <form className='form-container'>
 
           {isSignUp && (
             <label htmlFor="name">
@@ -34,14 +37,14 @@ const Auth = () => {
           </label>
 
           <label htmlFor="password">
-            <div>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
               <h4>Password</h4>
-              {!isSignUp && <h4>Forgot Password?</h4>}
+              {!isSignUp && <p style={{color: '#007ac6', fontSize: '13px'}}>Forgot Password?</p>}
             </div>
             <input type="password" name="password" id="password" />
 
             {isSignUp && 
-              <p> Passwords must contain at least eight <br/> characters, including at least 1 letter and 1 <br/> number.</p>
+              <p style={{ color: "#666767", fontSize: "13px"}}> Passwords must contain at least eight <br/> characters, including at least 1 letter and 1 <br/> number.</p>
             }
           </label>
 
@@ -49,7 +52,7 @@ const Auth = () => {
             isSignUp && (
               <label htmlFor='check'>
                 <input type="checkbox" name="check" id="check" />
-                <p>
+                <p style={{ fontSize: "13px"}}>
                   Opt-in to receive occasional <br/>
                   product updates, user research invitations,<br/>
                   company announcements, and digests.
@@ -65,12 +68,21 @@ const Auth = () => {
             }
           </button>
 
+          {isSignUp && (
+            <p style={{ color: "#666767", fontSize: "13px"}}>
+              By clicking to "Sign up", you agree to our 
+              <span style={{color: '#007ac6'}}> terms of <br/> service</span>, 
+              <span style={{color: '#007ac6'}}> privacy policy</span> and 
+              <span style={{color: '#007ac6'}}> cookie policy</span>.
+            </p>
+          )}
+
         </form>
 
         <p>
           {isSignUp
             ? "Already have an account?"
-            : "Don't have an accoint? "
+            : "Don't have an account? "
           }
 
           <button type='button' className='handle-switch-btn' onClick={handleSwitch}>
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='home-container'>
+      <LeftSideBar />
+      <div className='home-container-2'>
+        <HomeMainBar />
+        <RightSideBar />
+      </div>
     </div>
   )
 }
